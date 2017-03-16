835f07530f24c41f23dd285d85de90a92f07058a
allflame <taras.p.gyrnik@gmail.com>
2017-03-17 00:55:03 +0200

    Automaticall add and tag database services on config parsing

--- src/Extension/DatabaseExtension.php
+++ src/Extension/DatabaseExtension.php
@@ -13,7 +13,9 @@ declare(strict_types = 1);
 namespace Vainyl\Database\Extension;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 use Vainyl\Core\Extension\AbstractExtension;
+use Vainyl\Core\Extension\Exception\MissingRequiredFieldException;
 
 /**
  * Class DatabaseExtension
@@ -29,6 +31,24 @@ class DatabaseExtension extends AbstractExtension
     {
         $container
             ->addCompilerPass(new DatabaseCompilerPass());
+
+        foreach ($configs as $config) {
+            if (false === array_key_exists('databases', $config)) {
+                continue;
+            }
+
+            foreach ($config['databases'] as $name => $configData) {
+                if (false === array_key_exists('connection', $configData)) {
+                    throw new MissingRequiredFieldException($container, $name, $configData, 'connection');
+                }
+                $definition = (new Definition())
+                    ->setFactory(['database.factory.' . $configData['driver'], 'createDatabase'])
+                    ->setArguments([$name, $configData])
+                    ->addTag('database');
+
+                $container->setDefinition('database.' . $name, $definition);
+            }
+        }
 
         return parent::load($configs, $container);
     }
