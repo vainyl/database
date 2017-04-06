dc18a49f9ff1aae644a1823489248090af72a308
allflame <taras.p.gyrnik@gmail.com>
2017-04-06 19:32:35 +0300

    Fixed iterating over the attributes

--- src/Extension/DatabaseCompilerPass.php
+++ src/Extension/DatabaseCompilerPass.php
@@ -37,14 +37,11 @@ class DatabaseCompilerPass extends AbstractCompilerPass
 
         $services = $container->findTaggedServiceIds('database');
         foreach ($services as $id => $tags) {
-            foreach ($tags as $tag) {
-                if ('database' !== $tag['name']) {
-                    continue;
+            foreach ($tags as $attributes) {
+                if (false === array_key_exists('alias', $attributes)) {
+                    throw new MissingRequiredFieldException($container, $id, $attributes, 'alias');
                 }
-                if (false === array_key_exists('alias', $tag)) {
-                    throw new MissingRequiredFieldException($container, $id, $tag, 'alias');
-                }
-                $alias = $tag['alias'];
+                $alias = $attributes['alias'];
                 $definition = $container->getDefinition($id);
                 $inner = $id . '.inner';
                 $container->setDefinition($inner, $definition);
