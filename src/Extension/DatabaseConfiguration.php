6d97b28c3b359adfcd6c4533463f2f103cab7984
Taras Girnyk <taras.p.gyrnik@gmail.com>
2017-06-14 16:41:47 +0300

    Added synthetic logger

---
+++ src/Extension/DatabaseConfiguration.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Vainyl
+ *
+ * PHP Version 7
+ *
+ * @package   Connection
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://vainyl.com
+ */
+declare(strict_types=1);
+
+namespace Vainyl\Database\Extension;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * Class DatabaseConfiguration
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+class DatabaseConfiguration implements ConfigurationInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('databases');
+
+        $rootNode
+            ->useAttributeAsKey('name')
+            ->prototype('array')
+                ->children()
+                    ->scalarNode('connection')->end()
+                    ->arrayNode('options')->end()
+                    ->arrayNode('options')->end()
+                ->end()
+            ->end()
+        ;
+
+        return $treeBuilder;
+    }
+}
